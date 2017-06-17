c947e88ed8127f536b2fc01846db22d0bc1857a9
liangzhen6 <liangz@szmn.com.cn>
2017-06-17 14:28:55 +0800

    init

---
+++ Swift-03/Swift-03/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Swift-03
+//
+//  Created by liangzhen on 2017/6/17.
+//  Copyright © 2017年 liangzhen. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+
+class ViewController: UIViewController {
+
+    let path = Bundle.main.path(forResource: "sansheng", ofType: "mp4")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    @IBAction func palyAction(_ sender: UIButton) {
+        
+        let player = AVPlayer.init(url: NSURL.init(fileURLWithPath: path!) as URL)
+        
+        let playViewController = AVPlayerViewController()
+        playViewController.player = player
+        
+        self.present(playViewController, animated: true) { 
+            playViewController.player?.play()
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
